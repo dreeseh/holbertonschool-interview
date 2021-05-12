75cdc270a5d7612bd1a4749c3c2e9799587917f1
dreeseh <1859@holbertonschool.com>
2021-05-12 16:21:26 +0000

    added range 2, n

--- 0x03-minimum_operations/0-minoperations.py
+++ 0x03-minimum_operations/0-minoperations.py
@@ -19,14 +19,14 @@ def minOperations(n):
     # initial state
     result = 0
 
-    # return 0 if <=  1
+    # if n is impossible to achieve, return 0.
     if type(n) is not int or n <= 1:
         return result
 
     # iterate for the remaining numbers
     if n > 1:
-        for i in range(3, n + 1):
-            if n % i == 0:
+        for i in range(2, n):
+            while n % i == 0:
                 result += i
                 n = n / i
             else:
